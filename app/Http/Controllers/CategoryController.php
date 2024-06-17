d504d1ffc9cf91d1d29ad78d5e338e8c05a325d6
Sashok9203 <kovalchuk.olex@gmail.com>
2024-06-17 21:32:14 +0300

    Update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,22 +10,14 @@ use Intervention\Image\Drivers\Gd\Driver;
 class CategoryController extends Controller
 {
     protected string $upload;
-    protected string $url;
+
     public function __construct()
     {
-        $protocol = isset($_SERVER['HTTPS']) &&
-        $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
-        $base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
         $this-> upload = env('UPLOAD_DIR');
-        $this-> url = $base_url . $this->upload;
-
     }
     public function getall(): \Illuminate\Http\JsonResponse
     {
         $items = Category::all();
-        foreach ($items as $item) {
-            $item->image =$this->url . '600_'.$item->image;
-        }
         return response()->json($items) ->header('Content-Type', 'application/json; charset=utf-8');
     }
 
@@ -40,16 +32,13 @@ class CategoryController extends Controller
             $manager = new ImageManager(new Driver());
             $sizes = [50,150,300,600,1200];
             foreach ($sizes as $size) {
-                // read image from file system
                 $imageSave = $manager->read($file);
-                // resize image proportionally to 600px width
                 $imageSave->scale(width: $size);
                 $path = public_path($this->upload.$size."_".$fileName);
-                // save modified image in new format
                 $imageSave->toWebp()->save($path);
             }
             $item = Category::create(['name' => $request->input('name') , 'image' => $fileName]);
-            $item->image =$this->url . '600_'. $item->image;
+
             return response()->json($item, 201);
          }
         else
